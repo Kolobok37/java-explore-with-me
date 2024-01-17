9f00f7804b0b41de36187bde85a79aa2b73d356b
kolobok37 <kolobok373711@yandex.ru>
2024-01-17 20:12:19 +0300

    add empty ewm-service

---
+++ ewm-service/src/test/java/com/example/ewmservice/EwmServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ewmservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EwmServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

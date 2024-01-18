4961585e8a10eef4f92ff182e17f9470cce55bab
kolobok37 <kolobok373711@yandex.ru>
2024-01-18 19:16:48 +0300

    fix

--- stats-server/src/main/java/com/explore/exploreWithMe/model/Hit.java
+++ stats-server/src/main/java/com/explore/exploreWithMe/model/Hit.java
@@ -1,12 +1,12 @@
 package com.explore.exploreWithMe.model;
 
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity

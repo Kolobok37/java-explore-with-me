55424ddef72be57915e0b9226d494b2e52872229
kolobok37 <kolobok373711@yandex.ru>
2024-01-18 20:04:35 +0300

    fix

--- stats-server/src/main/java/com/explore/exploreWithMe/model/App.java
+++ stats-server/src/main/java/com/explore/exploreWithMe/model/App.java
@@ -1,11 +1,10 @@
 package com.explore.exploreWithMe.model;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotEmpty;
+import javax.persistence.*;
 import java.util.List;
 
 
@@ -17,11 +16,9 @@ import java.util.List;
 public class App {
     @Id
     @Column(name = "hit_uri")
-    @NotEmpty(message = "Uri is not be empty")
     private String uri;
 
     @Column(name = "name")
-    @NotEmpty(message = "Name app is not be empty")
     private String name;
 
     @OneToMany(cascade = CascadeType.ALL)
